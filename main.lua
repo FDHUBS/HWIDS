5176c63ad5c49c15a4a853bb86fd6603d3ea6bd8
FDHUBS <123385836+FDHUBS@users.noreply.github.com>
2023-01-24 01:46:11 +1100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,15 @@
+return {
+"be078b7771c09d6ec2bfe22fd472a97f3d87c8d89d1d2e2d6232c30f13ac15740d93bbd318647e90e048ca1fd4923981a231123625e93bb0e8a553b547e8b433";
+}
+return {
+"1";
+}
+return {
+"23";
+}
+return {
+"3";
+}
+return {
+"4";
+}
